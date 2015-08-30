d10d07ae83507656c11fac2281513a2c6dcfd08c
Chris Vandenberg <chrisvan0848@gmail.com>
2015-08-30 11:26:01 -0700

    modify

---
+++ main.js
@@ -0,0 +1,10 @@
+var myEl = document.getElementById('simpleDiv');
+
+myEl.addEventListener('click', function() {
+    alert('Hello world');
+});
+
+function sayWorking(){
+  console.log('working')
+}
+
